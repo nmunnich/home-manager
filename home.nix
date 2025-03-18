2b18ec687a117f888f518e39a5a025460df18c02
Nicolas Munnich <nickmunnich@gmail.com>
2025-03-18 10:24:44 +0100

    Initial commit

---
+++ home.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Import a module from the Home Manager repository
+    ./shell.nix
+  ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "nick";
+  home.homeDirectory = "/home/nick";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs;
+    [
+      # # Adds the 'hello' command to your environment. It prints a friendly
+      # # "Hello, world!" when run.
+      # pkgs.hello
+      lazygit
+
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+      # # You can also create simple shell scripts directly inside your
+      # # configuration. For example, this adds a command 'my-hello' to your
+      # # environment:
+      # (pkgs.writeShellScriptBin "my-hello" ''
+      #   echo "Hello, ${config.home.username}!"
+      # '')
+    ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/nick/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+    LESS = "-R --use-color -Dd+r$Du+b";
+    PATH = "$PATH:/home/nick/.local/bin:/home/nick/.nix-profile/bin";
+  };
+
+  # Enable the SSH agent service
+  # programs.ssh = { startAgent = true; };
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+  };
+  # From Librephoenix:
+  # Enable incoming ssh
+  # services.openssh = {
+  #   enable = true;
+  #   openFirewall = true;
+  #   settings = {
+  #     PasswordAuthentication = false;
+  #     PermitRootLogin = "no";
+  #   };
+  # };
+  # users.users.${userSettings.username}.openssh.authorizedKeys.keys = authorizedKeys;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,128 @@
+{ config, pkgs, ... }:
+let
+
+  # My shell aliases
+  myAliases = {
+    yay = "paru";
+    # TODO: make direnv
+    zmk-work =
+      "cd ~/Documents/Projects/Keyboards/zmk && source ./.venv/bin/activate";
+    prettier-zmk = "npm run prettier:format && npm run lint && npm run build";
+  };
+
+  # TODO: sway should be managed by home-manager, not via this.
+  initSway = ''
+    if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+        exec sway
+    fi
+  '';
+
+in {
+  imports = [
+    # Shell theming
+    ./oh-my-posh.nix
+  ];
+
+  # Extra packages
+  home.packages = with pkgs; [ direnv nix-direnv zsh-autosuggestions ];
+
+  programs = {
+    direnv.enable = true;
+    direnv.enableZshIntegration = true;
+    direnv.nix-direnv.enable = true;
+    command-not-found.enable = true;
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+    };
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+      # Causes this to replace cd
+      options = [ "--cmd cd" ];
+    };
+
+    ## FISH STUFF
+    # Bash only used as a login terminal, autostarts fish when new terminal is opened
+    bash = {
+      enable = true;
+      initExtra = ''
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+      '';
+      profileExtra = initSway;
+    };
+
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+      '';
+      shellAbbrs = myAliases;
+      plugins = [
+        # colorized command output
+        {
+          name = "grc";
+          src = pkgs.fishPlugins.grc.src;
+        }
+        # long command finish notifications
+        {
+          name = "done";
+          src = pkgs.fishPlugins.done.src;
+        }
+        # nice bash in fish
+        {
+          name = "bass";
+          src = pkgs.fishPlugins.bass.src;
+        }
+        # nicer git commands
+        {
+          name = "forgit";
+          src = pkgs.fishPlugins.forgit.src;
+        }
+        # no shell typos in history
+        {
+          name = "sponge";
+          src = pkgs.fishPlugins.sponge.src;
+        }
+      ];
+    };
+
+    # ZSH STUFF
+    zsh = {
+      enable = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      shellAliases = myAliases;
+      profileExtra = initSway;
+      history.size = 10000;
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "thefuck"
+          "alias-finder"
+          "extract"
+          "history-substring-search"
+          "command-not-found"
+          "pass"
+        ];
+      };
+
+      # TODO: fill this out properly during migration
+      dirHashes = {
+        doc = "$HOME/Documents";
+        pic = "$HOME/Pictures";
+        dl = "$HOME/Downloads";
+        keebs = "$HOME/Documents/Projects/Keyboards";
+      };
+    };
+  };
+}
